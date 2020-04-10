380ec33237e72e631e588e52ce38a820da55702c
chinmaishivaru <63471578+chinmaishivaru@users.noreply.github.com>
2020-04-11 00:12:19 +0530

    Update swap_chinmai.c

--- swap_chinmai.c
+++ swap_chinmai.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int a,b;
-printf("ENTER TWO NUMBERS A AND B\n");
-scanf("%d%d",&a,&b);
-printf("Before swapping A=%d and B=%d\n",a,b);
+int a,b; //Declaration of variables
+printf("ENTER TWO NUMBERS A AND B\n"); //Statement for users to enter two numbers
+scanf("%d%d",&a,&b); //Reading the values of a and b
+printf("Before swapping A=%d and B=%d\n",a,b); //Initial values displayed
 a=a+b;
 b=a-b;
-a=a-b;
-printf("After swapping A=%d and B=%d\n",a,b);
+a=a-b; //Operations to swap numbers
+printf("After swapping A=%d and B=%d\n",a,b); //Final values after swapping a and b
 return(0);
 }
