cf647ec0969a00b1a7fde653526899826085f73b
chinmaishivaru <63471578+chinmaishivaru@users.noreply.github.com>
2020-05-02 23:03:36 +0530

    Structure to store employee details.

---
+++ structure_employee.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef struct employee
+{
+  int empno;
+  char empname[25];
+  char department[20];
+  float salary;
+}emp;
+
+void print(emp k[2])
+{
+    for(int i=0;i<2;i++)
+    {
+        printf("Employee Number: %d\n",k[i].empno);
+        printf("Employee Name: %s\n",k[i].empname);
+        printf("Department: %s\n",k[i].department);
+        printf("Salary: %f\n",k[i].salary);
+    }
+}
+
+int main()
+{
+    emp a[20];
+    printf("Enter the details of employees\n");
+    for(int i=0;i<2;i++)
+    {
+        printf("Employee Number : ");
+        scanf("%d",&a[i].empno);
+        printf("\nEmployee Name : ");
+        scanf("%s",a[i].empname);
+        printf("\nDepartment : ");
+        scanf("%s",a[i].department);
+        printf("\nSalary : ");
+        scanf("%f\n",&a[i].salary);
+    }
+    print(a);
+}
